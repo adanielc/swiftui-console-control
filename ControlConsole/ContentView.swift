93eb5b9e76f1a93b7947edea3f1eec9d52ef3e7a
Scoresby <andresdaniel@gmail.com>
2022-04-24 18:43:39 +0200

    Initial Commit

---
+++ ControlConsole/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ControlConsole
+//
+//  Created by Daniel Castillo Bermudez on 24/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ControlConsole/ControlConsoleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ControlConsoleApp.swift
+//  ControlConsole
+//
+//  Created by Daniel Castillo Bermudez on 24/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct ControlConsoleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
