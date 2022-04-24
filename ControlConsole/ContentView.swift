29a40c528d60288b3ec7b7a75988a952ab0c9770
Scoresby <andresdaniel@gmail.com>
2022-04-24 20:15:18 +0200

    Warhammer Console Control First Commit

--- ControlConsole/ContentView.swift
+++ ControlConsole/ContentView.swift
@@ -8,10 +8,135 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State public var round: Int = 0;
+    @State public var playerOneCommandPoints: Int = 0;
+    @State public var playerTwoCommandPoints: Int = 0;
+    @State public var playerOneObjectivePoints: Int = 0;
+    @State public var playerTwoObjectivePoints: Int = 0;
+    
+    
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            ZStack{
+                VStack{
+                    VStack{
+                        Text("Ronda")
+                            .font(.title)
+                        HStack{
+                            Button(action: {round-=1}, label: {
+                                Image(systemName: "minus")
+                            })
+                            Text("\(round)")
+                                .bold()
+                                .font(.title)
+                            Button(action: {round+=1}, label: {
+                                Image(systemName: "plus")
+                            })
+                        }
+                    }
+                    Spacer()
+                    VStack{
+                        Text("Commnad Points")
+                            .font(.title)
+                        HStack{
+                            VStack{
+                                Text("Player 1")
+                                    .padding()
+                                    .font(.title2)
+                                HStack{
+                                    Button(action: {playerOneCommandPoints-=1}, label: {
+                                        Image(systemName: "minus")
+                                    })
+                                    Text("\(playerOneCommandPoints)")
+                                        .bold()
+                                        .font(.title)
+                                    Button(action: {playerOneCommandPoints+=1}, label: {
+                                        Image(systemName: "plus")
+                                    })
+                                }
+                            }
+                            Spacer()
+                            VStack{
+                                Text("Player 2")
+                                    .font(.title2)
+                                    .padding()
+                                HStack{
+                                    Button(action: {playerTwoCommandPoints-=1}, label: {
+                                        Image(systemName: "minus")
+                                    })
+                                    Text("\(playerTwoCommandPoints)")
+                                        .bold()
+                                        .font(.title)
+                                    Button(action: {playerTwoCommandPoints+=1}, label: {
+                                        Image(systemName: "plus")
+                                    })
+                                }
+                            }.padding()
+                        }
+                    }
+                    Spacer()
+                    VStack{
+                        Text("Objective Points")
+                            .font(.title)
+                            .padding()
+                        HStack{
+                            VStack{
+                                Text("Player 1")
+                                    .padding()
+                                    .font(.title2)
+
+                                HStack{
+                                    Button(action: {playerOneObjectivePoints-=1}, label: {
+                                        Image(systemName: "minus")
+                                    })
+                                    Text("\(playerOneObjectivePoints)")
+                                        .bold()
+                                        .font(.title)
+                                    Button(action: {playerOneObjectivePoints+=1}, label: {
+                                        Image(systemName: "plus")
+                                    })
+                                }
+                            }
+                            Spacer()
+                            VStack{
+                                Text("Player 2")
+                                    .font(.title2)
+                                    .padding()
+                                HStack{
+                                    Button(action: {playerTwoObjectivePoints-=1}, label: {
+                                        Image(systemName: "minus")
+                                    })
+                                    Text("\(playerTwoObjectivePoints)")
+                                        .bold()
+                                        .font(.title)
+                                    Button(action: {playerTwoObjectivePoints+=1}, label: {
+                                        Image(systemName: "plus")
+                                    })
+                                }
+                            }
+                        }
+                    }
+                    Spacer()
+                }
+            }
+            .background(
+            Image("background")
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                .opacity(0.3))
+            .navigationTitle("Control Console")
+            .navigationBarTitleDisplayMode(.inline)
+
+        }
+
+        
     }
+
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
